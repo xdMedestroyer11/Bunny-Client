9f29c15ab3361bed11b3c0c2b72fd211927d0dfe
xdMedestroyer11 <95560830+xdMedestroyer11@users.noreply.github.com>
2026-07-09 09:09:27 -0700

    Add client mixin template

---
+++ src/main/java/com/bunnyclient/mixin/ClientMixin.java
@@ -0,0 +1,9 @@
+package com.bunnyclient.mixin;
+
+import net.minecraft.client.main.Main;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Main.class)
+public class ClientMixin {
+    // Add custom mixins here for performance and PvP features
+}
